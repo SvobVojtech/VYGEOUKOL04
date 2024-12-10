5c81f24a3e96412fb9a911d6322527bb3d4afe9f
SvobVojtech <161499450+SvobVojtech@users.noreply.github.com>
2024-12-10 18:20:45 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o7e9b2M_Kkqgx_uNIgiLDE3KFFqKh0tvTHEoClENFzE/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1o7e9b2M_Kkqgx_uNIgiLDE3KFFqKh0tvTHEoClENFzE/edit?gid=603666405#gid=603666405';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
